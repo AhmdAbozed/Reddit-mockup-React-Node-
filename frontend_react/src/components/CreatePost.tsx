d852a936cb80744a476df2f6a387ea507113d412
ahmd hassan abozed <ahmdabozed88@gmail.com>
2022-10-30 21:11:10 +0200

    POST button is now disabled when any input is empty

--- frontend_react/src/components/CreatePost.tsx
+++ frontend_react/src/components/CreatePost.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./../css/CreatePost.css"
 const CreatePost = () => {
     
+    const [buttonState, setButtonState] = useState([0,0]);
+
+    useEffect(()=>{
+        const buttonElement = document.getElementById("create-post-submit");
+        console.log(buttonState)
+        if(buttonState[0] == 1 && buttonState[1] == 1){
+            buttonElement!.removeAttribute("disabled")
+            buttonElement!.setAttribute("style", "background-color: rgb(110,110,110)")
+        }
+        else{
+            buttonElement!.toggleAttribute("disabled")
+            buttonElement!.setAttribute("style", "background-color: rgb(222, 222, 222)")    
+        }
+    },[buttonState])
+
     const submitPost = async (event: React.FormEvent<HTMLFormElement>)=>{
+        
         event.preventDefault();
         const target = event.target as any
         const submission = {Title: target.title.value, Text: target.text.value}
+        
         const options = {
             method: "POST",
             headers:{
-    
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(submission)
         }
+
         const resp = await fetch("http://" + window.location.hostname + ":" + 3003 + "/posts", options);
         if(resp.status == 200){
             console.log("Mission success. 200")
@@ -29,13 +46,34 @@ const CreatePost = () => {
             <div className="create-post-item" id="create-post-head">
                 <a id="create-post-cancel" href="/"/>
                 <div id="create-post-title">Text</div>
-                <button id="create-post-submit">POST</button>
+                <button id="create-post-submit" disabled>POST</button>
             </div>
             <div id="create-post-item-container">
                 <div id="create-post-subreddit-icon"></div>
                 <div className="create-post-item" id="create-post-subreddit">r/subreddit</div>
-                <input className="create-post-item" type={"text"} name="title" id="create-post-title-input" placeholder="Add an interesting title"></input>
-                <textarea className="create-post-item" cols={1} rows={1} name="text" id="create-post-desc-input" placeholder="Add your text..."></textarea>
+                
+                <input className="create-post-item"
+                 type={"text"} name="title"
+                 id="create-post-title-input"
+                 placeholder="Add an interesting title"
+                 onChange={(e)=>{
+                    if(e.target.value !="")setButtonState([1, buttonState[1]]);
+                    else setButtonState([0, buttonState[1]]);}}
+                >
+                </input>
+
+                <textarea className="create-post-item"
+                 cols={1} rows={1}
+                 name="text" id="create-post-desc-input"
+                 placeholder="Add your text..."
+                 
+                 onChange={(e)=>{
+                    if(e.target.value !="")setButtonState([buttonState[0], 1])
+                    else setButtonState([buttonState[0], 0]);
+                    console.log(buttonState)}}
+                >
+                </textarea>
+
             </div>
         </form>
     )
--- frontend_react/src/components/SubredditItem.tsx
+++ frontend_react/src/components/SubredditItem.tsx
@@ -11,39 +11,14 @@ const Subreddit_Item = (props:{key: string,ID: string, Title: string, Members: s
 
     useEffect(()=>{(document.getElementById(props.ID+"votecount") as HTMLDivElement).innerHTML = JSON.stringify(voteCount);}, [voteCount])
 
-    function voteChange(event:React.MouseEvent<HTMLElement>){
-        
-        document.getElementById(props.ID+"upvote")?.classList.replace("filled", "hollow")
-        document.getElementById(props.ID+"downvote")?.classList.replace("filled", "hollow")
-
-        const voteElement = (event.target as HTMLButtonElement);
-        if(voteElement.classList.contains("upvote")){   
-            switch(voteState){
-                case "neutral": setCount(voteCount+1); voteElement.classList.replace("hollow", "filled"); setVote("upvoted");break;
-                case "upvoted": setCount(voteCount-1); voteElement.classList.replace("filled", "hollow"); setVote("neutral");break;
-                case "downvoted": setCount(voteCount+2); voteElement.classList.replace("hollow", "filled"); setVote("upvoted");
-            }
-        }
-        if(voteElement.classList.contains("downvote")){      
-            switch(voteState){    
-                case "neutral": setCount(voteCount-1); voteElement.classList.replace("hollow", "filled"); setVote("downvoted");break;
-                case "upvoted": setCount(voteCount-2);voteElement.classList.replace("hollow", "filled"); setVote("downvoted");break;
-                case "downvoted": setCount(voteCount+1); voteElement.classList.replace("filled", "hollow"); setVote("neutral");
-            }
-        }
-    }   
+    
     return (
         <div className= "post-item">
             <div  className="inline post-item-sub-icon" />
             <div className="post-item-subreddit">r/subreddit</div>
-            <div className="post-item-title">r/INSERT TITLE HERE</div>
+            <div className="post-item-title">r/SUBREDDIT</div>
             <img src="" alt="" className="post-item-img" />
-            <div className="post-item-details">
-                <input type={"button"} className="inline small-icon upvote hollow" id={props.ID + "upvote"} onClick={voteChange}/>
-                <div className="inline vote-count" id={props.ID + "votecount"}>99</div>
-                <div className="inline small-icon downvote hollow" id={props.ID + "downvote"} onClick={voteChange}></div>
-                <div className="inline awards-icon">|+|</div>
-            </div>
+            <div className="post-item-details"></div>
         </div>        
     )
 }
